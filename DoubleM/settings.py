851eb3f4aafe16d576a8ac3262208f8153df91f5
AbdelrahemanHamed <abdelrahemanhamed@gmail.com>
2025-09-16 23:55:21 +0300

    Fix INSTALLED_APPS for Heroku

--- DoubleM/settings.py
+++ DoubleM/settings.py
@@ -1,11 +1,24 @@
 INSTALLED_APPS = [
-    # default apps...
-    "rest_framework",
-    "accounts",
+    # Django default apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',  # <- needed for ContentType
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+
+    # Your apps
+    'accounts',
+    'teachers',
+    'courses',
+    'codes',
+    'quizzes',
 ]
 
 AUTH_USER_MODEL = "accounts.Student"
-
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
